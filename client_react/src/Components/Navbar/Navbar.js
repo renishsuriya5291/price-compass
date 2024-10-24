9636fdfa3a26a733933df29ccdf3bf60fda39e4b
renishsuriya5291 <renishsuriya1441@gmail.com>
2024-10-24 20:40:24 +0530

    msg

--- client_react/src/Components/Navbar/Navbar.js
+++ client_react/src/Components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Price Compass</Link>
+                <Link className="navbar-brand" to="/">Scrape and Enjoy! 😎</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -17,7 +17,7 @@ const Navbar = () => {
                         <li className="nav-item">
                             <Link className="nav-link" to="/reviews">Reviews</Link>
                         </li>
-                       
+
                     </ul>
                 </div>
             </div>
